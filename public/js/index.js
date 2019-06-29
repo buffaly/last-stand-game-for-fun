2bbb9c6ad5bf762deaecd41a5360eb046775743c
toey <anirut.k@cdg.co.th>
2019-06-29 17:33:14 +0700

    wating connect

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,39 @@
+function handleActions() {
+  $("#button-left").on("click", function() {
+    socket.emit("user_action", { action: "left" });
+  });
 
+  $("#button-up").on("click", function() {
+    socket.emit("user_action", { action: "up" });
+  });
 
-function test() {
-    console.log("HELLO WORLD");
+  $("#button-right").on("click", function() {
+    socket.emit("user_action", { action: "right" });
+  });
+}
+
+function handleRegister() {
+  $("#btn_submit_register").on("click", function() {
+    $(".register-box").attr("hidden", true);
+    socket.emit("init_user", {
+      username: $("#input_username").val(),
+      user_id: new Date().getTime()
+    });
+  });
+}
+
+function initilzeUser() {
+  const userId = window.localStorage.getItem("user_id");
+
+  if (!userId) {
+    return {
+      user: null,
+      isInit: false
+    };
+  }
+
+  return {
+    user: userId,
+    isInit: true
+  };
 }
--- src/app.js
+++ src/app.js
@@ -1,24 +1,49 @@
-const dotenv = require("dotenv");
-dotenv.config();
 const express = require("express");
 const path = require("path");
+
+// socket.io
+var server = require("http").Server(express);
+var io = require("socket.io")(server);
 const PORT = parseInt(process.env.PORT) || 3000;
-const firebase = require("./utils/firebase");
+const pathStatic = path.join(__dirname, "../public/");
+
+const Users = [];
+/*
+{
+  username: string,
+  user_id: timestamp,
+  score: string
+}
+*/
 
 main();
+
 function main() {
   const app = express();
-  const pathStatic = path.join(__dirname, "..", "public/");
-  app.use(express.static(pathStatic));
+  app.use(express.static(pathStatic))
+  server.listen(80);
+  io.on("connection", function(socket) {
+    socket.on("user_action", function(data) {
+      socket.broadcast.emit("user_action", data);
+    });
+    socket.on("init_user", data => {
+      const isDup = Users.find(user => user.user_id == data.user_id);
+      if (!isDup) {
+        data.score = 0;
+        Users.push(data);
+        console.log("user coming ", data);
+      } else
+        socket.broadcast.emit("recive_init_user", data);
+    });
+  });
+
   app.get("/", (req, res) => {
-    firebase.database().ref("test").push({
-      hello:"sss"
-    })
-    res.sendFile(pathStatic + "page1.html");
+    res.sendFile(pathStatic + "controller.html");
   });
-  app.get("/2", (req, res) => {
-    res.sendFile(pathStatic + "page2.html");
+  app.get("/game", (req, res) => {
+    res.sendFile(pathStatic + "game.html");
   });
+
   app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`);
   });
